645edde3042b591f1c8420eeb3826d8b397c34ef
Sarancha.Ponya.KKU <sarancha_ponya@kkumail.com>
2016-11-12 12:05:56 +0700

    Intent to signUp.

--- app/src/main/java/com/example/admin/saranchaponyakku/MainActivity.java
+++ app/src/main/java/com/example/admin/saranchaponyakku/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.admin.saranchaponyakku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUbButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUbButton = (Button) findViewById(R.id.button2);
+
+        //Sign Up
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+
+            }
+        });
+
+
+    }   // Main Method
+}   // Main Class
